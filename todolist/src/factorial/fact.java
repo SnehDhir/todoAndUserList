545921fb9e92b8e8d0492a4b57449e3027d6491c
SnehDhir <dhirsneh19@gmail.com>
2021-06-13 10:50:52 -0700

    List Of Users And TodoList

---
+++ todolist/src/factorial/fact.java
@@ -0,0 +1,19 @@
+class Factorial{
+    int factorial(int n){
+        if(n==0)
+        return 1;
+        else 
+        return (n*factorial(n-1));
+
+  
+        
+}
+public static void main(String args[]){
+    int i;
+    facti=1;
+    int no;
+    no=factorial();
+    System.out.println(+"factorial of number is"+no);
+    
+
+}
